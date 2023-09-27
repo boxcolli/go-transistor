0f62e82dfa846ce5e58211f2705c0d855ef0aebb
yoonmin <dmg04158@gmail.com>
2023-09-27 10:22:42 +0900

    update

--- wal/bolt/bolt_test.go
+++ wal/bolt/bolt_test.go
@@ -2,8 +2,19 @@ package bolt
 
 import (
 	"testing"
+
+	pb "github.com/boxcolli/pepperlink/idl/gen/hello/v1"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/encoding/protojson"
+	// "github.com/stretchr/testify/assert"
 )
 
 func TestBoltWAL(t *testing.T) {
-	
+	msg := pb.Hello{
+		Name: "Alice",
+	}
+	t.Log(protojson.Marshal(&msg))
+
+	msgByte, _ := proto.Marshal(&msg) // serialization: protobuf => []byte
+	t.Log(msgByte)
 }
