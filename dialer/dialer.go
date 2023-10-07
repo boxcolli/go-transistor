45780027916008ea3c7cdbe480700738bf10fb97
yoonmin <dmg04158@gmail.com>
2023-10-07 20:20:57 +0900

    comments on interfaces

--- dialer/dialer.go
+++ dialer/dialer.go
@@ -2,6 +2,7 @@ package dialer
 
 import "github.com/boxcolli/go-transistor/types"
 
+// Dialer takes care for grpc clients and their connections.
 type Dialer interface {
 	Dial(m types.Member)
 	Add(m types.Member)
--- emitters/emitter.go
+++ emitters/emitter.go
@@ -4,8 +4,9 @@ import (
 	"github.com/boxcolli/go-transistor/types"
 )
 
+// Emitter receives new messages and emits through outlet.
 type Emitter interface {
 	Start() <-chan error
 	Stop()
-	Emit(m *types.Message)
+	Emit(m *types.Message)	// goroutine safe; use channel to implement queue
 }
